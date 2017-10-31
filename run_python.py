e0a5ea09a7d3cae812c4980ddca276b2257587b3
Lydia <lydia_schuetz@web.de>
2017-10-31 18:37:17 +0000

    Improved documentation

--- run_python.py
+++ run_python.py
@@ -1,4 +1,9 @@
+# Import sys library
 import sys
 
+# Print that something else will be executed
+# \n indicates a new line
 print("\nRunning other file: \n")
+
+# Run the other file
 execfile("hello_world.py")
